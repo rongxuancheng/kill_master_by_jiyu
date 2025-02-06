c468e4d9d59da0a10b167e022349f179ac864ef2
rongxuancheng <atrong2022@outlook.com>
2025-02-06 21:37:17 +0800

    Add files via upload

---
+++ kill_master/kill_master/kill_master.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include<windows.h>
+using namespace std;
+int main()
+{
+    cout<<"please wait for 5 seconds"<<endl;
+    Sleep(5000);
+    system("taskkill /f /im studentmain.exe");
+    cout<<"Process killed successfully"<<endl;
+    system("pause");
+    return 0;
+}
